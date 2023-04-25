0026ad72d2f6ca51dc79fa0bde0a6c0f19d54ec0
PrangonGhose <prangonghose@gmail.com>
2023-04-25 23:55:45 +0600

    Add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,16 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp-relay.sendinblue.com',
+    port: 587,
+    user_name: 'nagorikkendrabd@gmail.com',
+    password: 'nwqah01UQ4cLXSAt',
+    authentication: 'login',
+    enable_starttls_auto: true
+  }
 end
