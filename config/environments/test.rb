11890f15e4713c97a72ab57506c4ec70abcb260e
PrangonGhose <prangonghose@gmail.com>
2023-04-29 16:25:01 +0600

    Add devise testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,9 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Add mailer to test
+
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
