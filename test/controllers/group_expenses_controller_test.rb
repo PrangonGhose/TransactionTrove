44f23ddee5e4373c7eabd6ac7638ffe028f92c38
PrangonGhose <prangonghose@gmail.com>
2023-04-29 16:24:14 +0600

    Auto generated test files

--- test/controllers/group_expenses_controller_test.rb
+++ test/controllers/group_expenses_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class GroupTest < ActiveSupport::TestCase
+class GroupExpensesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
